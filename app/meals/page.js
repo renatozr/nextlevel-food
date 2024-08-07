fd67e54d084f8ec2aff64cafda2c55cd01b3d53f
Renato Zanella Redoglia <renatozr07@gmail.com>
2024-08-07 18:28:00 +0000

    app

---
+++ app/meals/page.js
@@ -0,0 +1,43 @@
+import { Suspense } from "react";
+import Link from "next/link";
+
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import classes from "./page.module.css";
+
+export const metadata = {
+  title: "Browse Meals",
+  description: "Browse the delicious meals shared by our vibrant community.",
+};
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
---
+++ app/meals/share/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Share Meal",
+  description: "Engage with our community sharing your favorite meal!",
+};
+
+export default function ShareLayout({ children }) {
+  return children;
+}
